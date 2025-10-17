51092691a2810b7bf0c5630babcca2bef7e8ff7b
Shivani Takale <takaleshivani10@gmail.com>
2025-10-17 21:36:56 +0530

    Added code for search using iterative approach

--- src/main/java/org/example/divideAndConquer/RotatedSearchArray.java
+++ src/main/java/org/example/divideAndConquer/RotatedSearchArray.java
@@ -2,6 +2,7 @@ package org.example.divideAndConquer;
 
 public class RotatedSearchArray
 {
+    //using recursive approach
     public static int search(int[] arr, int target,int si, int ei)
     {
         int mid = si + (ei-si)/2;
@@ -34,11 +35,46 @@ public class RotatedSearchArray
 
     }
 
+    //Seach using iterative approach
+    public static int searchIterative(int[] arr,int target)
+    {
+        int mid=0;
+        int si = 0;
+        int ei=arr.length-1;
+
+        while(si<=ei)
+        {
+            mid = si +(ei-si)/2;
+            if(arr[mid] == target)
+                return mid;
+
+            //saerch in left half
+            if(arr[si] <= arr[mid])
+            {
+               // case a:search in left half
+               if(arr[si]<= target && target<=arr[mid])
+                   ei=mid-1;
+               else
+                   si=mid+1;
+            }
+            else //search in right half{
+            {
+                if(arr[mid]<=target && target<=arr[ei])
+                    si=mid+1;
+                else
+                    ei=mid-1;
+            }
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args)
     {
         int[] arr={4,5,6,7,0,1,2};
         int target=0;
-        int ans=search(arr,target,0, arr.length-1);
+//        int ans=search(arr,target,0, arr.length-1);
+        int ans=searchIterative(arr,target);
         System.out.println("Element found at index: "+ans);
     }
 }
